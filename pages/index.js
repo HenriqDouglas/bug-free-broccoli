1d4805265b979673e72ef33ded296b664614d0df
Henrique Abreu <henriquedouglassa@gmail.com>
2024-12-12 00:56:39 -0400

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Letícia, eu amo você. Se você me ama dá uma risadinha. Estou estudando pra melhorar nossa vida.</h1>
+  return (
+    <h1>
+      Letícia, eu amo você. Se você me ama dá uma risadinha. Estou estudando pra
+      melhorar nossa vida.
+    </h1>
+  );
 }
 
 export default Home;
