de842e40525e58e712efcae5d5f60df4e0bd8baa
Henrique Abreu <henriquedouglassa@gmail.com>
2024-12-12 23:30:41 -0400

    adicionando prósito a página `Em Construção`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 function Home() {
   return (
     <h1>
-      Letícia, eu amo você. Se você me ama dá uma risadinha. Estou estudando pra
-      melhorar nossa vida.
+      Estou, neste momento, construindo um novo espaço na internet para quem
+      vive e respira engenharia, seja em projetos inovadores ou na busca por
+      conhecimento prático. Aqui, seremos apaixonados pela precisão técnica e
+      pela colaboração, acreditando que a engenharia é tanto uma ciência quanto
+      uma arte compartilhada. Nosso termômetro de sucesso é simples: as pessoas
+      estão se conectando, colaborando e crescendo juntas? Hoje sou apenas 1 e
+      acredito neste projeto desde o Dia 0, continuarei contribuindo para moldar
+      um ambiente onde o conhecimento encontra a prática. Seja bem-vindo ao
+      começo de algo transformador!
     </h1>
   );
 }
