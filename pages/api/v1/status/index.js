3a83dfe07495f6df41ada5ba037bf2ab73cc9827
HenriqDouglas <30303173+HenriqDouglas@users.noreply.github.com>
2024-12-18 15:56:31 +0000

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ status: "Conexão realizada com sucesso!" });
+  const updatedAt = new Date().toISOString();
+
+  let postgreVersion = await database.query("SHOW server_version;");
+  postgreVersion = postgreVersion.rows[0].server_version;
+
+  let maxConnections = await database.query("SHOW max_connections;");
+  maxConnections = maxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  let usedConnections = await database.query({
+    text: "SELECT count(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  usedConnections = usedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        postgre_version: postgreVersion,
+        max_connections: parseInt(maxConnections),
+        used_connections: parseInt(usedConnections),
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,28 @@
 test("GET to /api/vi/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+
+  //verifica o status da resposta
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  //valida o campo `updated_at`
+  expect(responseBody.updated_at).toBeDefined();
+  const parsedUPdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBe(parsedUPdatedAt);
+
+  //valida a estrutura do campo `dependencies`
+  expect(responseBody.dependencies).toBeDefined();
+
+  //valida que o `postgre_version` segue o formato correto
+  const postgreVersion = responseBody.dependencies.database.postgre_version;
+  expect(postgreVersion).toEqual("16.6");
+
+  // Valida que `max_connections` e `used_connections` são números coerentes
+  const maxConnections = responseBody.dependencies.database.max_connections;
+  const usedConnections = responseBody.dependencies.database.used_connections;
+  
+  expect(maxConnections).toBe(100);
+  expect(usedConnections).toBe(1);
+  expect(usedConnections).toBeLessThanOrEqual(maxConnections); // Em uso <= máximo permitido
 });
